004985bb4683ed046636032bfb09c2a1fb97053a
garlicsauce <adrian.michalik@wp.pl>
2017-04-23 23:43:12 +0200

    initial commit with spring initalizr project

---
+++ src/main/java/pl/portfolio/garlicsauce/LearnTogetherApplication.java
@@ -0,0 +1,12 @@
+package pl.portfolio.garlicsauce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnTogetherApplication.class, args);
+	}
+}
