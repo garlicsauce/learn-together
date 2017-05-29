915fafa22f22479f1f0750463eeb7e78b2ea6270
garlicsauce <adrian.michalik@wp.pl>
2017-05-29 22:32:37 +0200

    replaced maven with gradle

--- src/main/java/portfolio/garlicsauce/learntogether/LearnTogetherApplication.java
+++ src/main/java/portfolio/garlicsauce/learntogether/LearnTogetherApplication.java
@@ -1,4 +1,4 @@
-package pl.portfolio.garlicsauce;
+package portfolio.garlicsauce.learntogether;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
