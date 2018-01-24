803687dea4107d297951fbb569736d233a3d2ae5
Adrian Michalik <adrian.michalik@hl.co.uk>
2018-01-24 21:38:33 +0100

    Security using Spring security and JWT
    + added authorization and authentication filters
    + added student details service
    + configured endpoints security
    + endpoint for getting students details
    + changed docker configuration - now docker-compose.app.yml allows you
    to 'up' environment WITH application and docker-compose.yml is
    responsible only for services that app depends on

---
+++ src/main/java/portfolio/garlicsauce/learntogether/security/JWTAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package portfolio.garlicsauce.learntogether.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import portfolio.garlicsauce.learntogether.student.Student;
+
+import java.io.IOException;
+import java.util.Date;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+
+        try {
+            Student student = new ObjectMapper().readValue(request.getInputStream(), Student.class);
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(student.getLogin(), student.getPassword()));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        String token = Jwts.builder()
+                .setSubject(((User)authResult.getPrincipal()).getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET.getBytes())
+                .compact();
+
+        response.addHeader(HttpHeaders.AUTHORIZATION, SecurityConstants.TOKEN_PREFIX + token);
+    }
+}
