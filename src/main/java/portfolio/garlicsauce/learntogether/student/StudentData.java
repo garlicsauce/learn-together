779e2a689d1972a7b1e639d2c8502ed093b561bc
Adrian Michalik <adrian.michalik@hl.co.uk>
2018-04-15 12:44:51 +0200

    Improve StudentController UT

--- src/main/java/portfolio/garlicsauce/learntogether/student/StudentData.java
+++ src/main/java/portfolio/garlicsauce/learntogether/student/StudentData.java
@@ -1,9 +1,11 @@
 package portfolio.garlicsauce.learntogether.student;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotBlank;
 
 @Data
 @Builder
@@ -11,8 +13,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class StudentData {
 
+    @JsonProperty(required = true)
+    @NotBlank
     private String username;
+
+    @JsonProperty(required = true)
+    @NotBlank
     private String password;
+
+    @JsonProperty(required = true)
+    @NotBlank
     private String firstName;
+
+    @JsonProperty(required = true)
+    @NotBlank
     private String lastName;
 }
--- src/test/groovy/portfolio/garlicsauce/learntogether/student/StudentControllerSpec.groovy
+++ src/test/groovy/portfolio/garlicsauce/learntogether/student/StudentControllerSpec.groovy
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header
@@ -16,15 +17,39 @@ class StudentControllerSpec extends Specification {
     def serviceMock = Mock(StudentService)
     def mockMvc = MockMvcBuilders.standaloneSetup(new StudentController(serviceMock)).build()
 
-    def "should POST /user and return CREATED status with Location header set"() {
+    def "should return CREATED status with Location header set on valid register user request"() {
         given:
-            def requestMock = Mock(StudentData)
+            def request = StudentData.builder()
+                    .username('jon_snow')
+                    .password('nightwatch123')
+                    .firstName('Jon')
+                    .lastName('Snow')
+                    .build()
         and:
             serviceMock.createStudent(_ as StudentData) >> Mock(Student) { getId() >> 1L }
 
         expect:
-            mockMvc.perform(post('/student').content(objectMapper.writeValueAsString(requestMock)).contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            mockMvc.perform(post('/student').content(objectMapper.writeValueAsString(request)).contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                     .andExpect(status().isCreated())
                     .andExpect(header().string('Location', '/student/1'))
+    }
+
+    @Unroll
+    def "should return BAD REQUEST status on request with missing data"() {
+        expect:
+            mockMvc.perform(post('/student').content(objectMapper.writeValueAsString(request)).contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                    .andExpect(status().isBadRequest())
+
+        where:
+            request << [
+                    new StudentData('', 'nightwatch123', 'Jon', 'Snow'),
+                    new StudentData('jon_snow', '', 'Jon', 'Snow'),
+                    new StudentData('jon_snow', 'nightwatch123', '', 'Snow'),
+                    new StudentData('jon_snow', 'nightwatch123', 'Jon', ''),
+                    new StudentData(null, 'nightwatch123', 'Jon', 'Snow'),
+                    new StudentData('jon_snow', null, 'Jon', 'Snow'),
+                    new StudentData('jon_snow', 'nightwatch123', null, 'Snow'),
+                    new StudentData('jon_snow', 'nightwatch123', 'Jon', null)
+            ]
     }
 }
